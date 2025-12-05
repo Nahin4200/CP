13a614dcbd028e7c0f2baa959cfe5c5cd6e81a00
Nahin4200 <nahinworld2024@gmail.com>
2025-12-05 20:27:15 +0600

    Create Fibonacci Series.cpp

---
+++ Fibonacci Series.cpp
@@ -0,0 +1,11 @@
+#include <bits/stdc++.h>
+using namespace std;
+int main(){ int n; 
+           cin>>n;
+           long long a=0,b=1; 
+    for(int i=0;i<n;i++){ 
+      cout<<a<<" ";
+      long long c=a+b;
+      a=b;
+      b=c;}
+}
