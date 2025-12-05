55c4abb4379a2dc61790db8cf35e9ab264bb38f6
Nahin4200 <nahinworld2024@gmail.com>
2025-12-05 20:25:11 +0600

    Create count digits.cpp

---
+++ count digits.cpp
@@ -0,0 +1,11 @@
+#include <bits/stdc++.h>
+using namespace std;
+int main(){ int n,c=0; 
+           cin>>n; 
+           while(n){ 
+             c++; 
+             n/=10;
+           }
+           cout<<c; 
+          }
+
