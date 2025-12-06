c1f4224baa36dc45033f0561b8f2b71c609910a2
Nahin4200 <nahinworld2024@gmail.com>
2025-12-06 21:53:28 +0600

    Create Print Unique Words in Sorted Order.cpp

---
+++ Print Unique Words in Sorted Order.cpp
@@ -0,0 +1,8 @@
+#include <bits/stdc++.h>
+using namespace std;
+int main(){
+    int n; cin>>n;
+    set<string> st;
+    while(n--){ string s; cin>>s; st.insert(s); }
+    for(auto &s:st) cout<<s<<" ";
+}
